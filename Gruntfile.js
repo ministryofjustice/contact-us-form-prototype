71fa78304398d026c73fbdf1546a149b590ec793
Jake Holland <DTU@Digitals-MacBook-Pro.local>
2016-10-14 10:56:37 +0100

    Update to prototype kit v4.0

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,25 +1,31 @@
-module.exports = function(grunt){
+module.exports = function (grunt) {
   grunt.initConfig({
-
     // Builds Sass
     sass: {
       dev: {
         options: {
-          style: "expanded",
-          sourcemap: true,
           includePaths: [
             'govuk_modules/govuk_template/assets/stylesheets',
             'govuk_modules/govuk_frontend_toolkit/stylesheets',
             'govuk_modules/govuk-elements-sass/'
           ],
-          outputStyle: 'expanded'
+          outputStyle: 'expanded',
+          sourceComments: true,
+          sourceMap: true
         },
         files: [{
           expand: true,
-          cwd: "app/assets/sass",
-          src: ["*.scss"],
-          dest: "public/stylesheets/",
-          ext: ".css"
+          cwd: 'app/assets/sass',
+          src: ['*.scss'],
+          dest: 'public/stylesheets/',
+          ext: '.css'
+        },
+        {
+          expand: true,
+          cwd: 'docs/assets/sass',
+          src: ['*.scss'],
+          dest: 'public/stylesheets/',
+          ext: '.css'
         }]
       }
     },
@@ -32,8 +38,13 @@ module.exports = function(grunt){
           cwd: 'app/assets/',
           src: ['**/*', '!sass/**'],
           dest: 'public/'
+        }, {
+          expand: true,
+          cwd: 'docs/assets/',
+          src: ['**/*', '!sass/**'],
+          dest: 'public/'
         }],
-        ignoreInDest: "**/stylesheets/**",
+        ignoreInDest: '**/stylesheets/**',
         updateAndDelete: true
       },
       govuk: {
@@ -73,14 +84,14 @@ module.exports = function(grunt){
         files: ['app/assets/sass/**/*.scss'],
         tasks: ['sass'],
         options: {
-          spawn: false,
+          spawn: false
         }
       },
-      assets:{
+      assets: {
         files: ['app/assets/**/*', '!app/assets/sass/**'],
         tasks: ['sync:assets'],
         options: {
-          spawn: false,
+          spawn: false
         }
       }
     },
@@ -105,33 +116,33 @@ module.exports = function(grunt){
         }
       }
     }
-  });
+  })
 
-  [
+  ;[
     'grunt-sync',
     'grunt-contrib-watch',
     'grunt-sass',
     'grunt-nodemon',
     'grunt-concurrent'
   ].forEach(function (task) {
-    grunt.loadNpmTasks(task);
-  });
+    grunt.loadNpmTasks(task)
+  })
 
   grunt.registerTask('generate-assets', [
     'sync',
     'sass'
-  ]);
+  ])
 
   grunt.registerTask('default', [
     'generate-assets',
     'concurrent:target'
-  ]);
+  ])
 
   grunt.registerTask(
     'test',
     'default',
     function () {
-      grunt.log.writeln('Test that the app runs');
+      grunt.log.writeln('Test that the app runs')
     }
-  );
-};
+  )
+}
