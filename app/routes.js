374262e447601e38db09f8e5ea8b0984b4564149
Jake Holland <jake.holland@digital.fco.gov.uk>
2016-10-18 13:43:35 +0100

    added behind the scenes suggested answer mock up

--- app/routes.js
+++ app/routes.js
@@ -20,6 +20,7 @@ var link = apiCall.apiResults
 
 
 
+
 // Branching
 
 router.get('/copy-check-your-answers-page',function (req, res){
@@ -110,6 +111,95 @@ res.render('confirmation-page', {'contact_email_display' : contact_email_display
 // res.render('copy-check-your-answers-page', {'country_display' : country_display,'contact_name_display' : contact_name_display, 'contact_email_display' : contact_email_display, 'enquirytext_display' : enquirytext_display }) 
 
 // })
+
+router.get('/suggested-articles-page',function (req, res){
+
+// get the answer from the query string (?fullnamename=john) and set it as a variable so you can use it  
+
+var contact_name_display = "Name"
+var contact_email_display = "Email Address"
+var enquirytext_display = "Enquiry Text"
+var country_display = "Italy"
+var results_passport = [
+{description: "You can apply for, update, renew or replace a passport online",link: "/apply-renew-passport",title: "Apply for, renew or replace a UK passport"},
+{description: "Apply online to renew your 10-year driving licence, full or provisional - cost, payment methods, documents and information you need",link: "/renew-driving-licence",title: "Renew your driving licence"},
+{description: "Renew or replace a passport urgently with the 1 day Premium service or the 1 week Fast Track service",link: "/get-a-passport-urgently",title: "Get a passport urgently"}
+] 
+var results_health = [
+{description: "Latest travel advice for Italy including safety and security, entry requirements, travel warnings and health",link: "/foreign-travel-advice/italy",title: "Italy travel advice"}, 
+{description: "EU emergency measures against Xylella fastidiosa and current issues related to protecting plant health and trade of plants, fruit, vegetables or plant material.",link: "/guidance/protecting-plant-health-topical-issues",title: "Protecting plant health: topical issues"},
+{description: "Latest travel advice for San Marino including safety and security, entry requirements, travel warnings and health",link: "/foreign-travel-advice/san-marino",title: "San Marino travel advice"}
+]
+var results_lifestyle = [{firstname:"life", lastname:"style"}]
+var health = ["health", "medicine", "medicines", "doctor"]
+var passport = ["passport", "lost", "renew", "Passport"]
+var lifestyle = ["lifestyle", "cost", "sunshine"]
+
+// var apiCall = require("./apiCall.js")
+
+  if (country_display == "Italy")
+  if (health.indexOf(enquirytext_display) > -1) {
+  // if (enquirytext_display.includes (health) == true) {
+
+    res.render('copy-check-your-answers-page', {'results_display': results_health, 'country_display' : country_display,'contact_name_display' : contact_name_display, 'contact_email_display' : contact_email_display, 'enquirytext_display' : enquirytext_display })
+    // redirect to the relevant page
+    // res.redirect('/italy-results');
+
+} else if (country_display == "Italy")
+  if (passport.indexOf(enquirytext_display) > -1) {
+  // if (enquirytext_display.includes (health) == true) {
+
+    res.render('copy-check-your-answers-page', {'results_display': results_passport, 'country_display' : country_display,'contact_name_display' : contact_name_display, 'contact_email_display' : contact_email_display, 'enquirytext_display' : enquirytext_display })
+    // redirect to the relevant page
+    // res.redirect('/italy-results');
+
+} else if (country_display == "Italy")
+  if (lifestyle.indexOf(enquirytext_display) > -1) {
+  // if (enquirytext_display.includes (health) == true) {
+
+    res.render('suggested-articles-page', {'results_display': results_lifestyle, 'country_display' : country_display,'contact_name_display' : contact_name_display, 'contact_email_display' : contact_email_display, 'enquirytext_display' : enquirytext_display })
+    // redirect to the relevant page
+    // res.redirect('/italy-results');
+
+
+} else {
+
+    // if over18 is any other value (or is missing) render the page requested
+    res.render('suggested-articles-page', {'results_display': results_health, 'country_display' : country_display,'contact_name_display' : contact_name_display, 'contact_email_display' : contact_email_display, 'enquirytext_display' : enquirytext_display }) 
+
+  }
+
+});
+
+router.get('/reply-page',function (req, res){
+
+// get the answer from the query string (?fullnamename=john) and set it as a variable so you can use it  
+
+var title_display = req.query.title
+var link_display = req.query.link
+var enquirytext_display = req.query.enquirytext
+var country_display = req.query.Country
+
+res.render('reply-page', {'title_display': title_display, 'link_display' : link_display})
+
+  
+
+});
+
+router.get('/reply-page2',function (req, res){
+
+// get the answer from the query string (?fullnamename=john) and set it as a variable so you can use it  
+
+var title_display = req.query.title
+var link_display = req.query.link
+var replytext_display = req.query.replytext
+var country_display = req.query.Country
+
+res.render('reply-page2', {'replytext_display': replytext_display, 'link_display' : link_display})
+
+  
+
+});
 
 
 module.exports = router;
